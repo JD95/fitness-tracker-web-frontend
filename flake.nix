9ed7e18e7a6e36c501f4ec56617c2fbda5689d51
Jeffrey Dwyer <jeffreydwyer95@outlook.com>
2023-08-08 17:51:01 +0000

    Fixes bundle step in nix build

--- flake.nix
+++ flake.nix
@@ -16,30 +16,39 @@
     let
       nixpkgs = inputs.nixpkgs.legacyPackages.${system};
       easy-purescript-nix = import inputs.easy-purescript-nix {pkgs = nixpkgs;};
-    in
-    {
-      packages.default = nixpkgs.stdenv.mkDerivation {
-        name = "fitness-tracker-frontend";
-        src = ./.;
-        nativeBuildInputs = [
+      pursTools = [
             easy-purescript-nix.purs-0_14_0
         ] ++ (
           spago2nix.packages.${system}.spago2nix_nativeBuildInputs {
             srcs-dhall = [./spago.dhall ./packages.dhall];
           }
         );
+
+    in
+    {
+      devShells.default = nixpkgs.mkShell {
+        packages = pursTools;
+      };
+
+      packages.default = nixpkgs.stdenv.mkDerivation {
+        name = "fitness-tracker-frontend";
+        src = ./.;
+        nativeBuildInputs = pursTools;
         unpackPhase = ''
           cp -r $src/src .
           cp $src/style.css .
           cp $src/index.html .
+          cp $src/spago.dhall .
+          cp $src/packages.dhall .
           install-spago-style
           '';
         buildPhase = ''
-          build-spago-style "./src/**/*.purs"
+          build-spago-style "./src/**/*.purs" 
+          purs bundle output/*/*.js -m Main --main Main -o index.js
           '';
         installPhase = ''
           mkdir -p $out
-          mv output $out/
+          mv index.js $out/
           mv index.html $out/
           mv style.css $out/
           '';
